0b63253a4ed71b715128d41d0de4c53c2f7acff0
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-03 11:28:08 -0300

    useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { HooksApp } from './HooksApp'
+import { CounterApp } from './01-useState/CounterApp'
+// import { HooksApp } from './HooksApp'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HooksApp />
+    <CounterApp />
   </StrictMode>,
 )
