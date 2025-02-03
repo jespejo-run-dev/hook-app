aa9b0998211672b6ec8097e7a74bd126e313dd4c
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-03 12:51:43 -0300

    Exponer metodos del Hook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
 
-export const useCounter = (initialValue = 10) => {
+export const useCounter = (initialValue = 5) => {
 
     const [counter, setCounter] = useState(initialValue);
 
+    const increment = (value = 1) => {
+        setCounter(counter + value)
+    };
+
+    const decrement = (value = 1) => {
+        setCounter(counter - value)
+    };
+
+    const reset = () => setCounter(initialValue);
+
     return {
-        counter
+        counter,
+        increment,
+        decrement,
+        reset
     }
 }
