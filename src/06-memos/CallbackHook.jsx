009f40ca83829df3dc1f973a33ebf7e135ce112a
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-05 12:39:47 -0300

    useCallback

---
+++ src/06-memos/CallbackHook.jsx
@@ -0,0 +1,36 @@
+import { useState, useCallback, useEffect } from 'react'
+import { ShowIncrement } from './ShowIncrement'
+
+export const CallbackHook = () => {
+
+    const [counter, setCounter] = useState(10);
+
+    const incrementFather = useCallback(
+      () => {       
+        setCounter((value) => value + 1);
+      },
+      [],
+    )
+
+    useEffect(() => {
+        incrementFather();
+    }, [incrementFather])
+    
+    
+
+    // const incrementFather = () => {
+    //     setCounter(counter + 1);
+    // }
+
+  return (
+    <>
+        <h1>CallbackHook Hook: {counter}</h1>
+        <hr />
+
+        <ShowIncrement
+            increment={incrementFather}
+        />
+
+    </>
+  )
+}
