32bbef658688c02b55fa5aeabef318d0a4300b86
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-06 12:33:34 -0300

    Agregar un nuevo TODO

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -12,7 +12,7 @@ const initialState = [
     },
     {
         id: new Date().getTime() * 3,
-        description: 'Recolectar la piedra del alma',
+        description: 'Recolectar la piedra del poder',
         done: false,
     },
 ]
@@ -22,7 +22,13 @@ export const TodoApp = () => {
     const [todos, dispatch] = useReducer(todoReducer, initialState)
 
     const handleNewTodo = (todo) => {
-        console.log(todo);
+
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo,
+        }
+
+        dispatch(action)
     }
 
   return (
