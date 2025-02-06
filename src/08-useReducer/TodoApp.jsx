a57bb60f146d85991b2bd64d022081a9fcf14faa
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-06 16:25:13 -0300

    Hook useTodos

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -1,63 +1,15 @@
-import { useReducer,useEffect } from 'react'
-import { todoReducer } from './todoReducer'
 import { TodoList } from './TodoList'
 import { TodoAdd } from './TodoAdd'
+import { useTodos } from '../hooks'
 
-const initialState = [
-    // {
-    //     id: new Date().getTime(),
-    //     description: 'Recolectar la piedra del alma',
-    //     done: false,
-    // },
-    // {
-    //     id: new Date().getTime() * 3,
-    //     description: 'Recolectar la piedra del poder',
-    //     done: false,
-    // },
-]
-
-const init = () => {
-    return JSON.parse(localStorage.getItem('todos')) || []
-}
 
 export const TodoApp = () => {
 
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
-
-    useEffect(() => {
-        localStorage.setItem('todos', JSON.stringify(todos))      
-    }, [todos])
-    
-
-    const handleNewTodo = (todo) => {
-
-        const action = {
-            type: '[TODO] Add Todo',
-            payload: todo,
-        }
-
-        dispatch(action)
-    }
-
-    const handleDeleteTodo = (id) => {
-        
-        dispatch({
-            type: '[TODO] Remove Todo',
-            payload: id,
-        })
-    }
-
-    const handleToggleTodo = (id) => {
-       
-        dispatch({
-            type: '[TODO] Toggle Todo',
-            payload: id,
-        })
-    }
+    const {todos, todosCount, pendingTodosCount,handleDeleteTodo, handleToggleTodo, handleNewTodo} = useTodos();
 
   return (
     <>
-        <h1>TodoApp 10, <small>pendientes: 2</small></h1>
+        <h1>TodoApp {todosCount}, <small>pendientes: {pendingTodosCount}</small></h1>
         <hr />
 
         <div className="row">
