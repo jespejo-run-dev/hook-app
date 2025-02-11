ef6de19ef99a7ed0e863ad2058cc9332ee165490
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-11 10:31:57 -0300

    Preparacion aplicacion con rutas

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router'
 import { createRoot } from 'react-dom/client'
 // import { CounterApp } from './01-useState/CounterApp'
 // import { HooksApp } from './HooksApp'
@@ -13,12 +14,14 @@ import { createRoot } from 'react-dom/client'
 // import { CallbackHook } from './06-memos/CallbackHook'
 // import { Padre } from './07-tarea-memo/Padre'
 // import './08-useReducer/intro-reducer'
-import { TodoApp } from './08-useReducer/TodoApp'
-
+// import { TodoApp } from './08-useReducer/TodoApp'
+import { MainApp } from './09-useContext/MainApp'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <TodoApp />
-  // </StrictMode>,
+  <BrowserRouter>
+  {/* <StrictMode> */}
+      <MainApp />
+  {/* </StrictMode>, */}
+  </BrowserRouter> 
 )
