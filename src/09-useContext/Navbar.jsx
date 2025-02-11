750c287c1e9ebf9decc07e41ba19ef9e0cf90105
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-11 11:03:44 -0300

    NavLink

--- src/09-useContext/Navbar.jsx
+++ src/09-useContext/Navbar.jsx
@@ -1,11 +1,38 @@
-import { Link } from 'react-router'
+import { Link, NavLink } from 'react-router'
 
 export const Navbar = () => {
   return (
-    <>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/login">Login</Link>
-    </>
+<nav className="navbar bg-dark navbar-expand-lg bg-body-tertiary rounded-3" data-bs-theme="dark">
+    <div className="container-fluid">
+
+        <Link className="navbar-brand" href="#">useContext</Link>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+
+                <NavLink 
+                    className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}
+                    to="/"
+                >
+                    Home
+                </NavLink>
+
+                <NavLink 
+                    className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}
+                    to="/about"
+                >
+                    About
+                </NavLink>
+
+                <NavLink 
+                    className={({isActive}) => `nav-link ${isActive ? 'active' : ''}`}
+                    to="/login"
+                >
+                    Login
+                </NavLink>
+            </ul>
+        </div>
+    </div>
+</nav>
   )
 }
