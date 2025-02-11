576bc0f9a36a4341dee9c11d465d3c9152b89b48
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-11 10:40:00 -0300

    Configurar Router en React

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router'
+import {  } from 'react-router'
+import { HomePage } from './HomePage'
+import { AboutPage } from './AboutPage'
+import { LoginPage } from './LoginPage'
 
 export const MainApp = () => {
     return (
         <>
             <h1>MainApp</h1>
             <hr />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="about" element={<AboutPage />} />
+
+                {/* Default */}
+                <Route path="/*" element={<Navigate to="/about" />} />
+
+            </Routes>
         </>
     )
 }
