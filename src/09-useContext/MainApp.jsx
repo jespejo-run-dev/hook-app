b4c0d9416e98c73d0f598d775320dfe46010bcce
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-11 11:14:31 -0300

    CreateContext y ContextProvider

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -5,10 +5,11 @@ import { HomePage } from './HomePage'
 import { AboutPage } from './AboutPage'
 import { LoginPage } from './LoginPage'
 import { Navbar } from './Navbar'
+import { UserProvider } from './context/UserProvider'
 
 export const MainApp = () => {
     return (
-        <>
+        <UserProvider>
             <Navbar />
             
             <hr />
@@ -23,6 +24,6 @@ export const MainApp = () => {
                 <Route path="/*" element={<Navigate to="/about" />} />
 
             </Routes>
-        </>
+        </UserProvider>
     )
 }
