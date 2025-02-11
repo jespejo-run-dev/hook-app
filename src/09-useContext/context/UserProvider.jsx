0ee7b8cb13dcd36fefe37be26ef95f8e473663cd
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-11 11:30:22 -0300

    useContext

--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext"
 
-const user = {
-  id: 123,
-  name: 'jespejo',
-  email: 'jespejo@gmail.com',
-}
+// const user = {
+//   id: 123,
+//   name: 'jespejo',
+//   email: 'jespejo@gmail.com',
+// }
 
 export const UserProvider = ({children}) => {
+
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{hola: 'Mundo', user: user}}>
+    // <UserContext.Provider value={{hola: 'Mundo', user: user}}>
+    <UserContext.Provider value={{ user, setUser }}>
         {children}
     </UserContext.Provider>
   )
