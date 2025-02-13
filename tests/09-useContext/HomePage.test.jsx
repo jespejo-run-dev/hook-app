ed8482e4994c12c6bf4ccbc057aba4587c2fa0c1
Jorge Espejo Ibacache <jorge.espejo.ibacache@gmail.com>
2025-02-13 11:36:29 -0300

    Pruebas en useContext

---
+++ tests/09-useContext/HomePage.test.jsx
@@ -0,0 +1,44 @@
+import { render, screen } from "@testing-library/react"
+import { HomePage } from "../../src/09-useContext/HomePage"
+import { UserContext } from "../../src/09-useContext/context/UserContext"
+
+describe('Pruebas en <HomePage />', () => {
+
+    const user = {
+        id: 1,
+        name: 'jespejo',
+    }
+
+    test('debe de mostrar el componente sin el usuario', () => {
+
+        render(
+            <UserContext.Provider value={{user: null}}>
+                <HomePage />
+            </UserContext.Provider>
+        )
+
+        const preTag = screen.getByLabelText('pre');
+        expect(preTag.innerHTML).toBe('null');
+        
+        // screen.debug();
+
+     })
+
+     test('debe de mostrar el componente con el usuario', () => {
+
+        render(
+            <UserContext.Provider value={{user}}>
+                <HomePage />
+            </UserContext.Provider>
+        )
+
+        screen.debug();
+
+        const preTag = screen.getByLabelText('pre');
+        expect(preTag.innerHTML).toContain(`${user.id}`);
+        expect(preTag.innerHTML).toContain(user.name);
+        
+
+     })
+
+})
